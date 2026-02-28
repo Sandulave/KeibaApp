20c7e336491660fc307d05d2e44847b054290375
Sandulave <sandulave@gmail.com>
2026-02-28 22:17:31 +0900

    sentry対応

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Session\TokenMismatchException;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,6 +23,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+
         $exceptions->render(function (TokenMismatchException $e, Request $request) {
             if ($request->isMethod('POST') && $request->routeIs('logout')) {
                 return redirect()->route('login');
