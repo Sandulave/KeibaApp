9d46fb4e213afafe961991f588fd7f33b041351a
Sandulave <sandulave@gmail.com>
2026-03-03 00:46:08 +0900

    翻訳出ないように修正

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" translate="no" class="notranslate">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Language" content="ja">
+    <meta name="google" content="notranslate">
     <title>メンテナンス中 | {{ config('app.name') }}</title>
     <style>
         :root {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="ja" translate="no" class="notranslate">
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="Content-Language" content="ja">
+  <meta name="google" content="notranslate">
   <title>Keiba App</title>
 </head>
 <body>
