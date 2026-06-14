1e81ec420d8705f174b10f372483ccb47e237496
Sandulave <sandulave@gmail.com>
2026-06-14 18:40:39 +0900

    アイコン修正

--- resources/views/components/site-logo.blade.php
+++ resources/views/components/site-logo.blade.php
@@ -6,11 +6,9 @@
 
 @if ($isSummerSite)
     @if ($variant === 'nav')
-        <div class="shrink-0 rounded-md border border-sky-200 bg-white p-1 shadow-sm">
-            <img src="{{ asset('summer_icon.png') }}" alt="夏競馬バトル" class="h-12 w-12 rounded" />
-        </div>
+        <img src="{{ asset('summer_icon.png') }}" alt="夏競馬バトル" class="h-14 sm:h-20 lg:h-24 w-auto shrink-0" />
     @else
-        <img src="{{ asset('summer_icon.png') }}" alt="夏競馬バトル" class="h-40 w-40 rounded-3xl sm:h-44 sm:w-44" />
+        <img src="{{ asset('summer_icon.png') }}" alt="夏競馬バトル" class="h-auto w-full max-w-xs" />
     @endif
 @else
     @if ($variant === 'nav')
